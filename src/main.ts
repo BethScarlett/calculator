ef15adca6f3f4091a7c6543dae050c0a36c5bce9
Bethany Burrows <jedcburrows@gmail.com>
2024-03-28 11:02:27 +0000

    Added event listeners to subtraction, multipication and division buttons. Implemented calculation functionality across addition, subtraction, multiplication and division.

--- src/main.ts
+++ src/main.ts
@@ -21,12 +21,17 @@ const nineButton = document.querySelector<HTMLButtonElement>("#nine");
 
 //Getting operator buttons
 const plusButton = document.querySelector<HTMLButtonElement>("#plus");
+const minusButton = document.querySelector<HTMLButtonElement>("#minus");
+const multiplyButton = document.querySelector<HTMLButtonElement>("#multiply");
+const divideButton = document.querySelector<HTMLButtonElement>("#divide");
 const equalsButton = document.querySelector<HTMLButtonElement>("#equals");
 
-//Initialising variables to store numbers for calculation
+//Initialising variables to store numbers & operators for calculation
 let storedNumOne: number = 0;
 let storedNumTwo: number = 0;
 let total: number = 0;
+let opToUse: string = "";
+let isFiring: boolean = false;
 
 //Handle any NULL exceptions by throwing errors if element doesn't exist
 //Null exceptions for number buttons
@@ -46,7 +51,13 @@ if (
 }
 
 //Null exceptions for operators
-if (!plusButton || !equalsButton) {
+if (
+  !plusButton ||
+  !minusButton ||
+  !multiplyButton ||
+  !divideButton ||
+  !equalsButton
+) {
   throw new Error("Error with operators");
 }
 
@@ -100,26 +111,94 @@ const getNine = (event: Event) => {
 //Event functions to perform calculations
 //When operator button is clicked the current display value should be stored but only if it's a number
 //If input is valid, a new number can be clicked
-//When equals or another operator is pressed previous operator calculation should be performed and stored if necessary
 //If input is invalid, an error should be displayed
+//When equals or another operator is pressed previous operator calculation should be performed and stored if necessary
 //Pressing clear should set display and stored numbers to zero
-let hasFired: boolean = false;
+
 const handleAddition = (event: Event) => {
   //console.log("Event: ", event);
-  if (!hasFired) {
-    storedNumOne = Number(numDisplay.innerText);
-    console.log(storedNumOne);
-    oprDisplay.innerText = "+";
-    hasFired = true;
-  } else {
-  }
+  storedNumOne = Number(numDisplay.innerText);
+  console.log("Number one is: " + storedNumOne);
+  oprDisplay.innerText = "";
+  oprDisplay.innerText = "+";
+  opToUse = "+";
+  isFiring = false;
 };
 
+const handleSubtraction = (event: Event) => {
+  //console.log("Event: ", event);
+  storedNumOne = Number(numDisplay.innerText);
+  console.log("Number one is: " + storedNumOne);
+  oprDisplay.innerText = "";
+  oprDisplay.innerText = "-";
+  opToUse = "-";
+  isFiring = false;
+};
+
+const handleMultiplication = (event: Event) => {
+  //console.log("Event: ", event);
+  storedNumOne = Number(numDisplay.innerText);
+  console.log("Number one is: " + storedNumOne);
+  oprDisplay.innerText = "";
+  oprDisplay.innerText = "*";
+  opToUse = "*";
+  isFiring = false;
+};
+
+const handleDivision = (event: Event) => {
+  //console.log("Event: ", event);
+  storedNumOne = Number(numDisplay.innerText);
+  console.log("Number one is: " + storedNumOne);
+  oprDisplay.innerText = "";
+  oprDisplay.innerText = "/";
+  opToUse = "/";
+  isFiring = false;
+};
+
+//When equals is pressed, it should take whatever is currently displayed and add it to the first stored number to output a total
+//If equals is pressed again, it should perform calculation again but with total instead of numOne
 const handleEquals = (event: Event) => {
   //console.log("Event: ", event);
-  total = total + storedNumOne;
+
+  if (!isFiring) {
+    isFiring = true;
+    storedNumTwo = Number(numDisplay.innerText);
+    console.log("Number two is :" + storedNumTwo);
+  } else {
+    console.log("firing");
+  }
+
+  switch (opToUse) {
+    case "+": {
+      console.log("Addition number one is: " + storedNumOne);
+      console.log("Addition number two is: " + storedNumTwo);
+      total = storedNumOne + storedNumTwo;
+      break;
+    }
+    case "-": {
+      console.log("Subtraction number one is: " + storedNumOne);
+      console.log("Subtraction number two is: " + storedNumTwo);
+      total = storedNumOne - storedNumTwo;
+      break;
+    }
+    case "*": {
+      console.log("Subtraction number one is: " + storedNumOne);
+      console.log("Subtraction number two is: " + storedNumTwo);
+      total = storedNumOne * storedNumTwo;
+      break;
+    }
+    case "/": {
+      console.log("Subtraction number one is: " + storedNumOne);
+      console.log("Subtraction number two is: " + storedNumTwo);
+      total = storedNumOne / storedNumTwo;
+      break;
+    }
+  }
   console.log(total);
   numDisplay.innerText = total.toString();
+  oprDisplay.innerText = "";
+  oprDisplay.innerText = "=";
+  storedNumOne = total;
 };
 
 //Add event listeners to number buttons to enable click
@@ -136,4 +215,7 @@ nineButton.addEventListener("click", getNine);
 
 //Add event listeners to operator buttons
 plusButton.addEventListener("click", handleAddition);
+minusButton.addEventListener("click", handleSubtraction);
+multiplyButton.addEventListener("click", handleMultiplication);
+divideButton.addEventListener("click", handleDivision);
 equalsButton.addEventListener("click", handleEquals);
